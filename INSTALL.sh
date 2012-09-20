000d85fadfc8b2c70081134ccf93d0b36a3a72a5
aptx4869 <ling548@gmail.com>
2012-09-20 15:25:48 +0800

    Initial Commit

---
+++ INSTALL.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#-------------------------------------------------
+#     Author: aptx4869 <ling548@gmail.com>
+#     https://github.com/aptx4869
+#
+#     File: INSTALL.sh
+#     Create Date: 2012-08-07 10:31:38
+#     Fork from :
+#         https://github.com/carlhuda/janus/blob/master/bootstrap.sh
+#     and https://github.com/robbyrussell/oh-my-zsh/blob/master/tools/install.sh
+#-------------------------------------------------
+
+date=$(date +%Y%m%d-%H%M%S)
+if [ -d ~/.tmux ];
+then
+	echo "\033[0;33mYou already have .tmux installed.\033[0m You'll need to remove ~/.tmux if you want to reinstall"
+	exit
+else
+	echo "\033[0;34mCloning tmux settings...\033[0m"
+	hash git >/dev/null && /usr/bin/env git clone --depth 1 --recursive git://github.com/aptx4869/oh-my-tmux.git ~/.tmux || {
+	echo "git not installed"
+	exit
+	}
+fi
+
+echo "\033[0;34mLooking for existing configs...\033[0m"
+
+for i in $HOME/.tmux.conf ; do
+	if [ -e $i ] || [ -h $i ]; then
+		echo "\033[0;33mFound ${i}\033[0m \033[0;32]Backing up to ${i}.bak.$date\033[0m";
+		mv "${i}" "${i}.bak.$date"
+	fi
+done
+
+echo "\033[0;34mLink tmux configuration files...\033[0m"
+TMUX_SETTINGS="$HOME/.tmux"
+for i in .tmux.conf ; do
+	echo "\033[0;33mLinking ${i}\033[0m \033[0;32] from ${HOME}/${i}\033[0m";
+	ln -s "${TmUX_SETTINGS}/${i}" "$HOME/${i}"
+done
---
+++ tmux-swap-pane.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+#-*-encoding:utf-8-*-
+
+#-------------------------------------------------
+#     Author: t9md  <taqumd@gmail.com>
+#     https://github.com/t9md
+#     from: https://github.com/t9md/tmux_config
+#     File: tmux-swap-pane.rb
+#     Copy Date: 2012-09-18 13:35:13
+#-------------------------------------------------
+
+def swap(src, dst)
+	system("tmux swap-pane -s#{src} -t#{dst}")
+end
+
+CONF="~/.tmux/lastpane"
+PANE_DIRECTION_TBL = {
+	:up => "U",
+	:down => "D",
+	:left => "L",
+	:right => "R",
+	:master => "M",
+}
+
+def swap_to(direction)
+	target = PANE_DIRECTION_TBL[direction]
+
+	org_pane=`tmux display-message -p "#P"`.chomp
+	system("tmux select-pane -#{target}")
+
+	dst_pane=`tmux display-message -p "#P"`.chomp
+	system("tmux select-pane -t#{org_pane}")
+
+	if target == "M"
+		dst_pane = 0
+	end
+	swap(*[ org_pane, dst_pane ])
+	# p [ org_pane, dst_pane ]
+	system("tmux select-pane -t#{dst_pane}")
+
+	# File.open(File.expand_path(CONF)).read # => ""
+	if target == "M"
+		dst_pane = 0
+		# File.open(File.expand_path(CONF),'w+').write(org_pane)
+	end
+end
+# def exchange
+
+# end
+
+# p ARGV[0].intern
+if PANE_DIRECTION_TBL.keys.include?(ARGV[0].intern)
+	swap_to ARGV[0].intern
+end
